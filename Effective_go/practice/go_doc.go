5320b0be9402be9a91686afab1d57ae86d02c4c2
Suriya Ganesh <suriyaganesh097@gmail.com>
2021-10-17 11:45:42 +0530

    effective go

---
+++ Effective_go/practice/go_doc.go
@@ -0,0 +1,8 @@
+/* 
+packkage regexp implements a simple library for regular expressions.
+
+The syntax of the regular expressions accepted is:
+
+*/
+
+package regexp
---
+++ Effective_go/practice/go_fmt.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+type T struct {
+	name  string // This is commanetsedsf
+	value int    //this is a comment
+}
